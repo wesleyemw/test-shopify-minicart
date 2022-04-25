29af2cac99aa3bd224c63dbc61a15c6af7ed9d14
Wesley Soares <wesley@reportsustentabilidade.com.br>
2022-04-25 13:22:37 -0300

    inserting language specfic tags and files

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,4 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    let offcanvasElement = document.querySelector( '.minicart-outer' );
-
-    document.addEventListener( 'click', function( event ) {
-        if ( event.target.matches( '.open-offcanvas' ) ) {
-            offcanvasElement.classList.toggle( 'closed' );
-            document.body.classList.toggle( 'no-scroll' );
-        }
-
-    });
-
-
-});
-
-/// inserting vue reactive and hoping 
+// inserting vue reactive and hoping 
 const store = Vue.reactive({
     state: {
         cartState: []
@@ -30,3 +14,20 @@ const store = Vue.reactive({
         })
     }
 })
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let offcanvasElement = document.querySelector( '.minicart-outer' );
+
+    document.addEventListener( 'click', function( event ) {
+        if ( event.target.matches( '.open-offcanvas' ) ) {
+            offcanvasElement.classList.toggle( 'closed' );
+            document.body.classList.toggle( 'no-scroll' );
+        }
+
+    });
+
+
+});
