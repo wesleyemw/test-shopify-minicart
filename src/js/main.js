35dcc85a19e595004f3bbac6446008d059ec82af
Wesley Soares <wesley@reportsustentabilidade.com.br>
2022-04-25 10:25:43 -0300

    adding to card

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 });
+
+/// inserting vue reactive and hoping 
+const store = Vue.reactive({
+    state: {
+        cartState: []
+    },
+
+    getCart() {
+    axios.get('/cart.js')
+        .then(response => {
+        this.state.cartState.unshift(response.data)
+        })
+        .catch(error => {
+        console.log(error)
+        })
+    }
+})
